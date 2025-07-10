99f4f0b4b00056f06355ccc03079c58bdd2c8bdc
Daniel Sanu <sanudaniel46@gmail.com>
2025-07-10 11:53:20 -1000

    added the position properties to Header.jsx

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -2,35 +2,45 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center px-20 py-4">
+    <header className="px-5 py-3 flex flex-col  justify-between items-center md:flex md:justify-between md:items-center lg:px-20 lg:py-3">
       {/* Logo */}
-      <div className="text-3xl text-zinc-700">Logo</div>
-      {/* Navbar */}
-      <nav>
-        <ul className="flex gap-15 text-zinc-700 text-[14px]">
-          <li className="cursor-pointer hover:text-black hover:scale-105 transition-all duration-300">
-            Home
-          </li>
-          <li className="cursor-pointer hover:scale-105 hover:text-black scale:text-[18px] transition-all duration-300">
-            Landlords
-          </li>
-          <li className="cursor-pointer hover:scale-105 hover:text-black scale:text-[18px] transition-all duration-300">
-            Erasmus Life
-          </li>
-          <li className="cursor-pointer hover:scale-105 hover:text-black scale:text-[18px] transition-all duration-300">
-            About Us
-          </li>
-        </ul>
-      </nav>
+      <div className="flex justify-between items-center w-full">
+        <div className="text-3xl text-zinc-700">Logo</div>
+        {/* Navbar */}
 
-      <div className="text-[14px] text-zinc-700 flex items-center gap-10 ">
-        <div className="cursor-pointer hover:scale-105 hover:text-black scale:text-[18px] transition-all duration-300">
-          Login
-        </div>
-        <button className="rounded-lg border border-zinc-400 px-3 py-2 text-[16px] font-semibold hover:bg-zinc-700 hover:text-white transition-all duration-500">
-          Rent a Room
+        {/* toggle button */}
+        <button className="relative flex flex-col lg:hidden items-center gap-1">
+          <div className="rounded p-0.5 bg-black w-[30px]"></div>
+          <div className="rounded p-0.5 bg-black w-[30px]"></div>
+          <div className="rounded p-0.5 bg-black w-[30px]"></div>
         </button>
       </div>
+      <nav >
+        <div className="absolute top-20 left-5 flex flex-col gap-5 lg:flex lg:gap-15 lg:text-zinc-700 ">
+          <ul className="flex flex-col gap-5">
+            <li className="cursor-pointer hover:text-black hover:scale-105 transition-all duration-300">
+              Home
+            </li>
+            <li className="cursor-pointer hover:scale-105 hover:text-black scale:text-[18px] transition-all duration-300">
+              Landlords
+            </li>
+            <li className="cursor-pointer hover:scale-105 hover:text-black scale:text-[18px] transition-all duration-300">
+              Erasmus Life
+            </li>
+            <li className="cursor-pointer hover:scale-105 hover:text-black scale:text-[18px] transition-all duration-300">
+              About Us
+            </li>
+          </ul>
+          <div className="flex flex-col gap-5 lg:text-zinc-700 lg:flex lg:gap-10 ">
+            <div className="cursor-pointer hover:scale-105 hover:text-black scale:text-[18px] transition-all duration-300">
+              Login
+            </div>
+            <button className="rounded-lg border border-zinc-400 px-3 py-2 text-[16px] font-semibold hover:bg-zinc-700 hover:text-white transition-all duration-500">
+              Rent a Room
+            </button>
+          </div>
+        </div>
+      </nav>
     </header>
   );
 }
