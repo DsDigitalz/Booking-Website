97a4171b834d09dc789e256c3ca43b808875be10
Daniel Sanu <sanudaniel46@gmail.com>
2025-07-09 23:24:42 -1000

    added styles to Header.jsx

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -7,17 +7,29 @@ export default function Header() {
       <div className="text-3xl text-zinc-700">Logo</div>
       {/* Navbar */}
       <nav>
-        <ul className="flex gap-10 text-zinc-700 text-[14px]">
-          <li>Home</li>
-          <li>Landlords</li>
-          <li>Erasmus Life</li>
-          <li>About Us</li>
+        <ul className="flex gap-15 text-zinc-700 text-[14px]">
+          <li className="cursor-pointer hover:text-black hover:scale-105 transition-all duration-300">
+            Home
+          </li>
+          <li className="cursor-pointer hover:scale-105 hover:text-black scale:text-[18px] transition-all duration-300">
+            Landlords
+          </li>
+          <li className="cursor-pointer hover:scale-105 hover:text-black scale:text-[18px] transition-all duration-300">
+            Erasmus Life
+          </li>
+          <li className="cursor-pointer hover:scale-105 hover:text-black scale:text-[18px] transition-all duration-300">
+            About Us
+          </li>
         </ul>
       </nav>
 
-      <div className="text-[14px] text-zinc-700 flex items-center gap-10">
-        Login
-        <button className="rounded-lg border border-zinc-400 px-3 py-2 text-[16px] font-semibold ">Rent a Room</button>
+      <div className="text-[14px] text-zinc-700 flex items-center gap-10 ">
+        <div className="cursor-pointer hover:scale-105 hover:text-black scale:text-[18px] transition-all duration-300">
+          Login
+        </div>
+        <button className="rounded-lg border border-zinc-400 px-3 py-2 text-[16px] font-semibold hover:bg-zinc-700 hover:text-white transition-all duration-500">
+          Rent a Room
+        </button>
       </div>
     </header>
   );
