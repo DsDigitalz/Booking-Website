0942e30f583c861cad977592e56b7da02a120c6e
Daniel Sanu <sanudaniel46@gmail.com>
2025-07-10 20:57:06 -1000

    added a shadow at Header.jsx

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="px-5 py-3 flex  justify-between items-center md:flex md:justify-between md:items-center lg:px-20 lg:py-3">
+    <header className="shadow px-5 py-3 flex  justify-between items-center md:flex md:justify-between md:items-center lg:px-10 lg:py-3">
       {/* Logo */}
       <div className="flex justify-between items-center w-full lg:w-fit">
         <div className="text-3xl text-zinc-700">Logo</div>
@@ -16,8 +16,8 @@ export default function Header() {
         </button>
       </div>
       <nav>
-        <div className="p-7 bg-white shadow-2xl absolute top-12 right-5 flex flex-col gap-5 items-center lg:gap-15 lg:text-zinc-700 lg:static lg:bg-transparent lg:shadow-none lg:p-0 lg:w-auto  lg:items-center">
-          <ul className="lg:flex  lg:gap-10 ">
+        <div className="p-10 bg-white shadow-2xl absolute top-12 right-5 flex flex-col gap-7 lg:gap-25 xl:gap-25  items-center lg:flex lg:justify-between lg:text-zinc-700 lg:static lg:bg-transparent lg:shadow-none lg:p-0 lg:w-auto  lg:items-center lg:flex-row">
+          <ul className="flex  flex-col gap-10 lg:flex-row  lg:gap-10  ">
             <li className="cursor-pointer hover:text-black hover:scale-105 transition-all duration-300">
               Home
             </li>
@@ -31,14 +31,14 @@ export default function Header() {
               About Us
             </li>
           </ul>
-          {/* <div className="flex flex-col gap-5 lg:text-zinc-700 lg:flex lg:gap-10 ">
-            <div className="cursor-pointer hover:scale-105 hover:text-black scale:text-[18px] transition-all duration-300">
+          <div className=" lg:text-zinc-700 lg:flex lg:gap-10 lg:items-center">
+            <div className=" ml-4 cursor-pointer hover:scale-105 hover:text-black scale:text-[18px] transition-all duration-300 lg:ml-0">
               Login
             </div>
-            <button className="rounded-lg border border-zinc-400 px-3 py-2 text-[16px] font-semibold hover:bg-zinc-700 hover:text-white transition-all duration-500">
+            <button className="mt-5  rounded-lg border border-zinc-400 px-3 py-2 text-[16px] font-semibold hover:bg-zinc-700 hover:text-white transition-all duration-500 lg:mt-0">
               Rent a Room
             </button>
-          </div> */}
+          </div>
         </div>
       </nav>
     </header>
