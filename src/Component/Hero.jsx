751cecc532d30a0a949f6bc04f6e6c7baad03093
Daniel Sanu <sanudaniel46@gmail.com>
2025-07-11 23:25:29 -1000

    added a positioning container in Hero,jsx

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -2,19 +2,27 @@ import React from "react";
 
 export default function Hero() {
   return (
-    <section className="flex flex-col gap-5">
+    <section className="relative flex flex-col gap-5">
+      {/* Hero image */}
       <div>
         <img src="highhouse.png" alt="" />
       </div>
+      {/* Hero info */}
       <div className="flex flex-col items-center text-[40px]">
         <div className="flex flex-col leading-11  ">
-        <h1 className="text-blue-600 font-semibold">Find Your Future</h1>
-        <h1 className="text-[30px] font-semibold text-center">Dream Accomodation</h1>
+          <h1 className="text-blue-600 font-semibold">Find Your Future</h1>
+          <h1 className="text-[30px] font-semibold text-center">
+            Dream Accomodation
+          </h1>
         </div>
         <p className="mt-2 text-[16px] text-center w-[90%]">
           Want to find an accommodation? We are ready to help you find one that
           suits your lifestyle and needs.{" "}
         </p>
+      </div>
+      {/* Position container */}
+      <div className="absolute">
+        
       </div>
     </section>
   );
