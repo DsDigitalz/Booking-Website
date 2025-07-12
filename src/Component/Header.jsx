ddc5dd255297a81a7a5d1bff1ff2a225e4971a10
Daniel Sanu <sanudaniel46@gmail.com>
2025-07-11 22:20:13 -1000

    added animation to Header.jsx

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 
 export default function Header() {
-// Create a useState hook to manage the toggle state
-const[isOpen, setIsOpen] = useState(false)
-// Function to toggle the menu
-function toggleMenu() {
-  setIsOpen((prev)=>(!prev))
-
-} 
+  // Create a useState hook to manage the toggle state
+  const [isOpen, setIsOpen] = useState(false);
+  // Function to toggle the menu
+  function toggleMenu() {
+    setIsOpen((prev) => !prev);
+  }
 
   return (
     <header className="shadow px-5 py-3 flex  justify-between items-center md:flex md:justify-between md:items-center lg:px-10 lg:py-3">
@@ -17,15 +16,40 @@ function toggleMenu() {
         {/* Navbar */}
 
         {/* toggle button */}
-        <button onClick={toggleMenu} className="relative flex flex-col lg:hidden items-center gap-1">
-          <div className="rounded p-0.5 bg-black w-[30px]"></div>
-          <div className="rounded p-0.5 bg-black w-[30px]"></div>
-          <div className="rounded p-0.5 bg-black w-[30px]"></div>
+        <button
+          onClick={toggleMenu}
+          className="relative flex flex-col lg:hidden items-center gap-1"
+        >
+          <div
+            className={`px-3.5 py-0.5 rounded bg-black  transition-all ${
+              isOpen ? "translate-y-2 rotate-45" : ""
+            }`}
+          ></div>
+          <div
+            className={`px-3.5 py-0.5 rounded bg-black  transition-opacity ${
+              isOpen ? "opacity-0" : "opacity-100"
+            }  `}
+          ></div>
+          <div
+            className={`px-3.5 py-0.5 rounded bg-black transition-all ${
+              isOpen ? "-translate-y-2 -rotate-45" : ""
+            }`}
+          ></div>
         </button>
       </div>
-      <nav className={`${isOpen ? "translate-x-0" : "translate-x-full"} lg:block transition-all duration-300 ease-in-out`}>
+      <nav>
         {/* Mobile Menu */}
-        <div className="p-10 bg-white shadow-2xl absolute top-12 right-5 flex flex-col gap-7 lg:gap-25 xl:gap-25  items-center lg:flex lg:justify-between lg:text-zinc-700 lg:static lg:bg-transparent lg:shadow-none lg:p-0 lg:w-auto  lg:items-center lg:flex-row">
+        <div
+          className={`lg:static lg:translate-x-0 transition-transform duration-500 ease-in-out ${
+            isOpen ? "translate-x-0 " : "translate-x-full"
+          } absolute top-12 right-0 w-[60%] z-20  overflow-hidden
+           p-10 bg-white shadow-2xl  flex flex-col gap-7
+          lg:gap-25 xl:gap-25 items-center lg:flex lg:justify-between
+          lg:text-zinc-700 lg:static lg:bg-transparent lg:shadow-none lg:p-0
+          lg:w-auto lg:items-center lg:flex-row
+          
+          `}
+        >
           <ul className="flex  flex-col gap-10 lg:flex-row  lg:gap-10  ">
             <li className="cursor-pointer hover:text-black hover:scale-105 transition-all duration-300">
               Home
