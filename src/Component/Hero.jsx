6f1bcf584c8013cd4894147e779a71ab4fc11385
Daniel Sanu <sanudaniel46@gmail.com>
2025-07-11 23:22:16 -1000

    added info in Her.jsx

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -7,11 +7,11 @@ export default function Hero() {
         <img src="highhouse.png" alt="" />
       </div>
       <div className="flex flex-col items-center text-[40px]">
-        <div>
+        <div className="flex flex-col leading-11  ">
         <h1 className="text-blue-600 font-semibold">Find Your Future</h1>
-        <h1 className="text-[30px] font-semibold">Dream Accomodation</h1>
+        <h1 className="text-[30px] font-semibold text-center">Dream Accomodation</h1>
         </div>
-        <p className="text-[16px] text-center w-[90%]">
+        <p className="mt-2 text-[16px] text-center w-[90%]">
           Want to find an accommodation? We are ready to help you find one that
           suits your lifestyle and needs.{" "}
         </p>
