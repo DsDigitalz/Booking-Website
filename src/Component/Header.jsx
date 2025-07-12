ba82128abc7325247ba946779fc876dad64b6a86
Daniel Sanu <sanudaniel46@gmail.com>
2025-07-11 21:55:34 -1000

    added useState and function to Header.jsx

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Header() {
+// Create a useState hook to manage the toggle state
+const[isOpen, setIsOpen] = useState(false)
+// Function to toggle the menu
+function toggleMenu() {
+  setIsOpen((prev)=>(!prev))
+
+} 
+
   return (
     <header className="shadow px-5 py-3 flex  justify-between items-center md:flex md:justify-between md:items-center lg:px-10 lg:py-3">
       {/* Logo */}
@@ -9,13 +17,14 @@ export default function Header() {
         {/* Navbar */}
 
         {/* toggle button */}
-        <button className="relative flex flex-col lg:hidden items-center gap-1">
+        <button onClick={toggleMenu} className="relative flex flex-col lg:hidden items-center gap-1">
           <div className="rounded p-0.5 bg-black w-[30px]"></div>
           <div className="rounded p-0.5 bg-black w-[30px]"></div>
           <div className="rounded p-0.5 bg-black w-[30px]"></div>
         </button>
       </div>
-      <nav>
+      <nav className={`${isOpen ? "translate-x-0" : "translate-x-full"} lg:block transition-all duration-300 ease-in-out`}>
+        {/* Mobile Menu */}
         <div className="p-10 bg-white shadow-2xl absolute top-12 right-5 flex flex-col gap-7 lg:gap-25 xl:gap-25  items-center lg:flex lg:justify-between lg:text-zinc-700 lg:static lg:bg-transparent lg:shadow-none lg:p-0 lg:w-auto  lg:items-center lg:flex-row">
           <ul className="flex  flex-col gap-10 lg:flex-row  lg:gap-10  ">
             <li className="cursor-pointer hover:text-black hover:scale-105 transition-all duration-300">
