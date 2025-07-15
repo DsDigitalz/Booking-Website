8ea87709e871a981d947f75fb2113723e230c680
Daniel Sanu <sanudaniel46@gmail.com>
2025-07-14 14:13:26 -1000

    added a new image to About.jsx

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      
-    </div>
+    <section className='max-w-[1440px] mx-auto flex flex-col '>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </section>
   )
 }
