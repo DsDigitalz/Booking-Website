9523091f1cdee48dbe8c59ac807471323e03b331
Daniel Sanu <sanudaniel46@gmail.com>
2025-07-14 14:06:31 -1000

    created About.Jsx Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Component/Header'
 import Hero from './Component/Hero'
+import About from './Component/About'
 
 export default function App() {
   return (
     <div>
       <Header/>
       <Hero/>
+      <About/>
     </div>
   )
 }
