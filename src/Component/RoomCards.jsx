aef51b2775f5fad303e309385886f82a88a7c0ae
Daniel Sanu <sanudaniel46@gmail.com>
2025-07-15 12:53:08 -1000

    added images and icons to cards

--- src/Component/RoomCards.jsx
+++ src/Component/RoomCards.jsx
@@ -2,9 +2,28 @@ import React from "react";
 
 export default function RoomCards() {
   return (
-    <section className="max-w-[1440px] mt-20">
-      <div>
-        
+    <section className="max-w-[1440px] mt-20 px-10">
+      {/* Card Heading */}
+      <div className=" flex justify-center">
+        <h1 className="font-semibold text-3xl text-center">
+          Reserve the Finest Rooms
+        </h1>
+      </div>
+      {/* Cards info */}
+      <div className="mt-10">
+        <div>
+          <img src="/card1.png" alt="" />
+        </div>
+        <div className="border border-zinc-100 shadow  p-4">
+          <div className="flex justify-between items-center">
+            <h2 className="text-xl font-medium">Riverside Retreat</h2>
+            <img src="Heart.png" alt="" className="w-7" />
+          </div>
+            <p className="mt-4">123 Avenida da Liberdade, Lisbon</p>
+            <div>
+                <p>500€</p>
+            </div>
+        </div>
       </div>
     </section>
   );
