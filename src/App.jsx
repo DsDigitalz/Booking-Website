9f99417c9e7e815633dcfacbf010d30236b9d471
Daniel Sanu <sanudaniel46@gmail.com>
2025-07-15 16:27:16 -1000

    added Process.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import Header from './Component/Header'
-import Hero from './Component/Hero'
-import About from './Component/About'
-import RoomCards from './Component/RoomCards'
-import Testimonials from './Component/Testimonials'
+import React from "react";
+import Header from "./Component/Header";
+import Hero from "./Component/Hero";
+import About from "./Component/About";
+import RoomCards from "./Component/RoomCards";
+import Testimonials from "./Component/Testimonials";
+import Process from "./Component/Process";
 
 export default function App() {
   return (
     <div>
-      <Header/>
-      <Hero/>
-      <About/>
-      <RoomCards/>
-      <Testimonials/>
+      <Header />
+      <Hero />
+      <About />
+      <RoomCards />
+      <Testimonials />
+      <Process />
     </div>
-  )
+  );
 }
